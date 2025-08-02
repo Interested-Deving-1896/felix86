a1157ec745ad6d033b421978cb16d58857d53607
Paris Oplopoios <parisoplop@gmail.com>
2025-08-02 17:27:27 +0300

    Proper error handling for unknown thunks and some signatures (#295)

--- src/felix86/common/overlay.cpp
+++ src/felix86/common/overlay.cpp
@@ -1,3 +1,5 @@
+#include <fcntl.h>
+#include "felix86/common/elf.hpp"
 #include "felix86/common/global.hpp"
 #include "felix86/common/log.hpp"
 #include "felix86/common/overlay.hpp"
@@ -15,14 +17,23 @@ void Overlays::addOverlay(const char* lib_name, const std::filesystem::path& des
     overlays.push_back({lib_name, dest});
 }
 
-const char* Overlays::isOverlay(const char* pathname) {
+const char* Overlays::isOverlay(int fd, const char* pathname) {
     std::filesystem::path path = pathname;
+    if (path.is_relative()) {
+        if (fd != AT_FDCWD) {
+            path = std::filesystem::path("/proc/self/fd/" + std::to_string(fd)) / path;
+        }
+    } else {
+        path = g_config.rootfs_path / path.relative_path();
+    }
     std::string filename = path.filename();
-
     for (auto& entry : overlays) {
         if (filename == entry.lib_name) {
-            LOG("Found overlay %s -> %s", pathname, entry.overlayed_path.c_str());
-            return entry.overlayed_path.c_str();
+            Elf::PeekResult result = Elf::Peek(path);
+            if ((g_mode32 && result == Elf::PeekResult::Elf32) || (!g_mode32 && result == Elf::PeekResult::Elf64)) {
+                LOG("Found overlay %s -> %s", pathname, entry.overlayed_path.c_str());
+                return entry.overlayed_path.c_str();
+            }
         }
     }
 
